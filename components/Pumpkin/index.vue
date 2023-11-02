3a722838c99f3ea3fc5721edc3f578e5d1ed2a2a
NeoPrint3D <drew@ronsman.com>
2023-11-01 21:51:37 -0500

    Add pumpkin mesh and custom shader material

--- components/Pumpkin/index.vue
+++ components/Pumpkin/index.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <!-- <TresMesh :geometry="points.geometry" :material="points.material" /> -->
+</template>
 
 <script setup lang="ts">
 import {
@@ -27,50 +29,45 @@ context.scene.value.add(new AmbientLight(0x404040));
 
 // add a box
 
-useGLTF("/pumpkin.glb").then((logo) => {
-  console.log(logo);
-  // convert object3d to geometry
+const logo = await useGLTF("/pumpkin.glb");
+console.log(logo);
+// convert object3d to geometry
 
-  logo.nodes.Pumpkin.geometry?.scale(0.025, 0.025, 0.025);
+logo.nodes.Pumpkin.geometry?.scale(0.025, 0.025, 0.025);
 
-  const pointsParentGeometry = logo.nodes.Pumpkin.geometry?.clone();
+const pointsParentGeometry = logo.nodes.Pumpkin.geometry?.clone();
 
-  const baseMaterial = new PointsMaterial({
-    color: "#ffffff",
-    size: 0.00625,
-    sizeAttenuation: true,
-  });
+const baseMaterial = new PointsMaterial({
+  color: "#ffffff",
+  size: 0.00625,
+  sizeAttenuation: true,
+});
 
-  // const customShader = new
+// const customShader = new
 
-  const shaderMaterial = new CustomShaderMaterial({
-    baseMaterial,
-    vertexShader: patchShaders(vertexShader),
-    fragmentShader: patchShaders(fragmentShader),
-    uniforms: {
-      uTime: { value: 0 },
-      // get the window size
-      uResolution: {
-        value: new Vector3(window.innerWidth, window.innerHeight, 1),
-      },
+const shaderMaterial = new CustomShaderMaterial({
+  baseMaterial,
+  vertexShader: patchShaders(vertexShader),
+  fragmentShader: patchShaders(fragmentShader),
+  uniforms: {
+    uTime: { value: 0 },
+    // get the window size
+    uResolution: {
+      value: new Vector3(window.innerWidth, window.innerHeight, 1),
     },
-  });
+  },
+});
 
-  const points = new Points(pointsParentGeometry, shaderMaterial);
+const points = new Points(pointsParentGeometry, shaderMaterial);
 
-  context.scene.value.add(points);
+context.scene.value.add(points);
 
-  const light = new PointLight(0xffffff, 1, 100, 1);
+const { onLoop } = useRenderLoop();
 
-  context.scene.value.add(light);
+onLoop(({ elapsed }) => {
+  points.rotation.y += 0.005;
 
-  const { onLoop } = useRenderLoop();
-
-  onLoop(({ elapsed }) => {
-    points.rotation.y += 0.005;
-
-    points.position.y = Math.sin(elapsed * 0.75) * 0.125;
-    shaderMaterial.uniforms.uTime.value += 0.001;
-  });
+  points.position.y = Math.sin(elapsed * 0.75) * 0.125;
+  shaderMaterial.uniforms.uTime.value += 0.001;
 });
 </script>
